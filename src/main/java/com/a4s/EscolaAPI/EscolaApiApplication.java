cc823cfa78e08769c22bdac6faa08cd0b2cfd077
robinsonmourao <49078615+robinsonmourao@users.noreply.github.com>
2022-09-30 15:14:02 -0300

    Initial commit

---
+++ src/main/java/com/a4s/EscolaAPI/EscolaApiApplication.java
@@ -0,0 +1,14 @@
+package com.a4s.EscolaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolaApiApplication.class, args);
+	}
+
+}
+//https://www.youtube.com/watch?v=9SGDpanrc8U&t=370s
---
+++ src/test/java/com/a4s/EscolaAPI/EscolaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a4s.EscolaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
